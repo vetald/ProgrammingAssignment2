9ab174c3925c3b0a6fb288c38b7797bd1e124553
vetald <vetald@yandex.ru>
2014-07-24 20:17:27 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## For this assignment, assume that the matrix supplied is always invertible.
+## structure to cash data
 
 makeCacheMatrix <- function(x = matrix()) {
+  xinv <- NULL
 
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(inv) xinv <<- inv
+  getinv <- function() xinv
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
+## return cash inverse matrix or calculate cash inverse matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  xinv <- x$getinv()
+  if(!is.null(xinv)) {
+    message("getting cached data")
+    return(xinv)
+  }
+  data <- x$get()
+  xinv <- solve(data, ...)
+  x$setinv(xinv)
+  xinv        
 }
+
+## test to perform
+## a<-makeCacheMatrix(matrix(c(1,2,2,2), 2,2))
+## cacheSolve(a)
